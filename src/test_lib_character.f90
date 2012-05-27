385f8ec2480e08bd8ff95a4dedd852a2f6e3eab4
khkh <thisisdummy@gmail.com>
2012-05-27 23:08:50 +0900

    New: StaticCms.
    mkdir my_site
    cd my_site
    thor sc:init
    rake

---
+++ src/test_lib_character.f90
@@ -0,0 +1,15 @@
+program test_lib_character
+  use lib_character, only: str, STR_LEN
+  implicit none
+  character(len = STR_LEN):: buf
+  buf = trim(str(huge(3.0d0)))
+  print*, trim(buf)
+  print*, trim(str(3))
+  print*, trim(str(-3))
+  print*, trim(str(3.0))
+  print*, trim(str((3.0, 4.0)))
+  print*, trim(str((3.0, 4.0d0)))
+  print*, trim(str(.true.))
+  print*, trim(str(.false.))
+  stop
+end program test_lib_character
