83824a0ae8b4892ee4ad2f20836cab555edd56d7
khkh <thisisdummy@gmail.com>
2012-06-26 15:46:03 +0900

    Modify test_lib_io.f90.

--- src/test_lib_io.f90
+++ src/test_lib_io.f90
@@ -4,10 +4,16 @@ program test_lib_io
   implicit none
   real, allocatable:: A(:, :)
   real, allocatable:: I(:)
+  real:: d(1:2, 1:3)
+  integer:: j, k
 
   print*, VERSION, ARRAY_EXT, META_EXT
-!  call write_array('int', (/1,2,3,4,5/), "what am i?")
-!  call write_array('r_desc', reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/)), "real with desc test.", "desc2 is also added.")
+  call write_array('int', (/1,2,3,4,5/), "what am i?")
+  call write_array('r_desc', reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/)), "real with desc test.", "desc2 is also added.")
+  d = reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/))
+  do j = 1, 2
+    write(6, *) (d(j, k), k = 1, 3)
+  end do
   call read_array('r_desc', A)
   print*, A
   print*, shape(A)
