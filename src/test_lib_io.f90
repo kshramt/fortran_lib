c5c4c83557c2b23bcae4b33128494ee6a5f092fc
kshramt <thisisdummy@gmail.com>
2012-07-11 05:15:20 +0900

    Modify lib_io.
    1) Rename VERSION to LIB_IO_VERSION.
    2) read/write_array stores data into ./NAME.array/ directory instead of NAME.array and NAME.array.meta files.
       This change ease handling (copy, delete and so on) of array data.
       You can just copy or delete the NAME.array directory instead of the two files.
    3) VERSION is renamed to LIB_IO_VERSION and stored in version.dat instead of meta data file.
    4) Deal with a case where ERROR/INPUT/OUTPUT_UNIT have all negative value.
    5) If an allocated array is passed to the read_unit, it is deallocated and reallocated.
       Is this behavior appropriate?

--- src/test_lib_io.f90
+++ src/test_lib_io.f90
@@ -1,5 +1,5 @@
 program test_lib_io
-  use lib_io, only: VERSION, ARRAY_EXT, META_EXT, write_array, read_array
+  use lib_io, only: LIB_IO_VERSION, write_array, read_array
 
   implicit none
   real, allocatable:: A(:, :)
@@ -7,7 +7,7 @@ program test_lib_io
   real:: d(1:2, 1:3)
   integer:: j, k
 
-  print*, VERSION, ARRAY_EXT, META_EXT
+  print*, LIB_IO_VERSION
   call write_array('int', (/1,2,3,4,5/), "what am i?")
   call write_array('r_desc', reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/)), "real with desc test.", "desc2 is also added.")
   d = reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/))
