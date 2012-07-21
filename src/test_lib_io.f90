1f4650fd55158baec937321dfd4b4be4a2a5d5d6
kshramt <thisisdummy@gmail.com>
2012-07-21 23:36:13 +0900

    Fix test_lib_io.

--- src/test_lib_io.f90
+++ src/test_lib_io.f90
@@ -8,18 +8,18 @@ program test_lib_io
   integer:: j, k
 
   print*, LIB_IO_VERSION
-  call write_array('int', (/1,2,3,4,5/), "what am i?")
-  call write_array('r_desc', reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/)), "real with desc test.", "desc2 is also added.")
+  call write_array('int.array', (/1,2,3,4,5/), "what am i?")
+  call write_array('r_desc.array', reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/)), "real with desc test.", "desc2 is also added.")
   d = reshape((/1.0, 2.0, 3.0, 4.0, 5.0, 6.0/), (/2, 3/))
   do j = 1, 2
     write(6, *) (d(j, k), k = 1, 3)
   end do
-  call read_array('r_desc', A)
+  call read_array('r_desc.array', A)
   print*, A
   print*, shape(A)
 
-  call read_array('int', I)
+  call read_array('int.array', I)
   print*, I
   print*, shape(I)
-  call read_array('int', A)
+  call read_array('int.array', A)
 end program test_lib_io
