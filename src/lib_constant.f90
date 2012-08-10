9c2411bd8c56a82dc2aa996a39fd9eecac5ed531
kame1 <kame1@kame1.(none)>
2012-08-10 17:41:32 +0900

    Use real(REAL64) instead of double precision.

--- src/lib_constant.f90
+++ src/lib_constant.f90
@@ -1,9 +1,10 @@
 module lib_constant
+  use iso_fortran_env, only: REAL32, REAL64
   implicit none
   private
   public:: PI, DEG_TO_RAD, RAD_TO_DEG
+  real(REAL64), parameter:: PI = 4*atan(1.0d0)
+  real(REAL64), parameter:: DEG_TO_RAD = PI/180
+  real(REAL64), parameter:: RAD_TO_DEG = 180/PI
 
-  double precision, parameter:: PI = 4*atan(1.0d0)
-  double precision, parameter:: DEG_TO_RAD = PI/180
-  double precision, parameter:: RAD_TO_DEG = 180/PI
 end module lib_constant
