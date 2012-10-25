ddb98ad71aefe90d2c3bc0a6b5694a9a4b5fbd24
kshramt <thisisdummy@example.com>
2012-10-26 00:38:26 +0900

    Use utils.h. lib_utils is no longer required.

--- Rakefile
+++ Rakefile
@@ -1,10 +1,3 @@
-ERB_COMPILER = 'erb'
-ERB_OPTIONS = "-T '-' -P"
-ERBED_F90_FILES = FileList['src/*.f90.erb'].ext('')
-
-desc "Compile erb files."
-task erb: ERBED_F90_FILES
-
-rule '.f90' => '.f90.erb' do |t|
-  sh "#{ERB_COMPILER} #{ERB_OPTIONS} #{t.source} > #{t.name}"
+task :compile do
+  cd('src'){system 'rake erb'}
 end
---
+++ src/utils.h
@@ -0,0 +1,27 @@
+#ifndef UTILS_HAVE_ALREADY_LOADED
+#  define UTILS_HAVE_ALREADY_LOADED
+#  define USE_UTILS_H use iso_fortran_env, only: ERROR_UNIT
+#  define WHERE_AM_I __FILE__, " ", __LINE__
+#  define WARN(...) write(ERROR_UNIT, *) "WARN: ", WHERE_AM_I, ##__VA_ARGS__
+#  define RAISE(...) write(ERROR_UNIT, *) "RAISE: ", WHERE_AM_I, ##__VA_ARGS__; stop 1
+#  define MUST_NOT_HAPPEN RAISE('Must not happen.') /* Must not happen unless a compiler has bugs. */
+#  define BUG RAISE('Bug.')			  /* Should not happen unless some programs have bugs. */
+
+#  define WARN_IF(isBad, ...)\
+     if(isBad)then;\
+       WARN(#isBad, " ", ##__VA_ARGS__);\
+     end if
+
+#  define RAISE_IF(isBad, ...)\
+     if(isBad)then;\
+       RAISE(#isBad, " ", ##__VA_ARGS__);\
+     end if
+
+#  ifdef DEBUG
+#    define DEBUG_PRINT(...) print*, WHERE_AM_I, ##__VA_ARGS__
+#    define DEBUG_PRINT_VARIABLE(x) print*, WHERE_AM_I, #x, x /* Print a variable name and it's value. */
+#  else
+#    define DEBUG_PRINT(...)
+#    define DEBUG_PRINT_VARIABLE(x)
+#  endif
+#endif
