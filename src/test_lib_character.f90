24183de1c16f65aea3a7830ed29a6a14a359027a
kshramt <thisisdummy@example.com>
2012-11-10 18:56:40 +0900

    Add Character `+' Character so that you can join strings within macro expression.

--- src/test_lib_character.f90
+++ src/test_lib_character.f90
@@ -1,15 +1,15 @@
 program test_lib_character
-  use lib_character, only: s, str, STR_LEN
+  use lib_character, only: s, str, STR_LEN, operator(+), operator(*)
   implicit none
   print*, STR_LEN
-  print*, '|' // str(huge(3.0d0)) // '|'
-  print*, '|' // str(3) // '|'
-  print*, '|' // str(-3) // '|'
-  print*, '|' // str(3.0) // '|'
-  print*, '|' // str((3.0, 4.0)) // '|'
-  print*, '|' // str((3.0, 4.0d0)) // '|'
-  print*, '|' // str(.true.) // '|'
-  print*, '|' // str(.false.) // '|'
-  print*, '|' // s(" abc d  ") // '|'
+  print*, '|' + str(huge(3.0d0)) + '|'
+  print*, '|' + str(3) + '|'
+  print*, '|' + str(-3) + '|'
+  print*, '|' + str(3.0) + '|'
+  print*, '|' + str((3.0, 4.0)) + '|'
+  print*, '|' + str((3.0, 4.0d0)) + '|'
+  print*, '|' + str(.true.) + '|'
+  print*, '|' + str(.false.) + '|'
+  print*, '|' + s(" abc d  ") + '|'
   stop
 end program test_lib_character
