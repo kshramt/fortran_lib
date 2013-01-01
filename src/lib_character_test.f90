cfa6878d03c00eb2e6a024b1e20ac8173eb5f61b
kshramt <thisisdummy@example.com>
2013-01-01 19:05:17 +0900

    Simplify `utils.h'
    * Remove __VA_ARGS__ to improve portability
    * Downcase names of function-like macros
    * Add missing `OUTPUT_UNIT' for `USE_UTILS_H'
    * Add `quote' macro to improve portability

--- src/lib_character_test.f90
+++ src/lib_character_test.f90
@@ -7,37 +7,37 @@ program lib_character_test
   implicit none
 
   ! s
-  TEST(s('') == '')
-  TEST(s('a') == 'a')
-  TEST(s(' ') == '')
-  TEST(s(' a') == 'a')
-  TEST(s('a ') == 'a')
-  TEST(s(' a ') == 'a')
-  TEST(s(' a b ') == 'a b')
+  test(s('') == '')
+  test(s('a') == 'a')
+  test(s(' ') == '')
+  test(s(' a') == 'a')
+  test(s('a ') == 'a')
+  test(s(' a ') == 'a')
+  test(s(' a b ') == 'a b')
 
   ! str
-  TEST(str('a') == 'a')
-  TEST(str(0) == '0')
-  TEST(str(-1) == '-1')
-  TEST(str(0.0) == '0.00000000')
-  TEST(str(1.0) == '1.00000000')
-  TEST(str(0.0_REAL64) == '0.0000000000000000')
-  TEST(str(-1.0_REAL64) == '-1.0000000000000000')
-  TEST(str((1.0, -1.0)) == '(  1.00000000    , -1.00000000    )')
-  TEST(str(.true.) == 'T')
-  TEST(str(.false.) == 'F')
+  test(str('a') == 'a')
+  test(str(0) == '0')
+  test(str(-1) == '-1')
+  test(str(0.0) == '0.00000000')
+  test(str(1.0) == '1.00000000')
+  test(str(0.0_REAL64) == '0.0000000000000000')
+  test(str(-1.0_REAL64) == '-1.0000000000000000')
+  test(str((1.0, -1.0)) == '(  1.00000000    , -1.00000000    )')
+  test(str(.true.) == 'T')
+  test(str(.false.) == 'F')
 
   ! +
-  TEST('' + '' == '')
-  TEST('' + 'a' == 'a')
-  TEST('a' + '' == 'a')
-  TEST('a' + 'b' == 'ab')
-  TEST(' a ' + 'b' == ' a b')
+  test('' + '' == '')
+  test('' + 'a' == 'a')
+  test('a' + '' == 'a')
+  test('a' + 'b' == 'ab')
+  test(' a ' + 'b' == ' a b')
 
   ! *
-  TEST('a'*0 == '')
-  TEST('a'*1 == 'a')
-  TEST('a'*2 == 'aa')
+  test('a'*0 == '')
+  test('a'*1 == 'a')
+  test('a'*2 == 'aa')
 
   write(OUTPUT_UNIT, *) "SUCCESS: ", __FILE__
   stop
--- src/utils.h
+++ src/utils.h
@@ -1,39 +1,40 @@
 #ifndef UTILS_HAVE_ALREADY_LOADED
 #  define UTILS_HAVE_ALREADY_LOADED
-#  define USE_UTILS_H use, intrinsic:: iso_fortran_env, only: ERROR_UNIT
+#  define USE_UTILS_H use, intrinsic:: iso_fortran_env, only: ERROR_UNIT, OUTPUT_UNIT
 #  define WHERE_AM_I __FILE__, " ", __LINE__
-#  define WARN(...) write(ERROR_UNIT, *) "WARN: ", WHERE_AM_I, ##__VA_ARGS__
-#  define RAISE(...) write(ERROR_UNIT, *) "RAISE: ", WHERE_AM_I, ##__VA_ARGS__; stop 1
-#  define MUST_NOT_HAPPEN RAISE('Must not happen.') /* Must not happen unless a compiler has bugs. */
-#  define BUG RAISE('Bug.')			  /* Should not happen unless some programs have bugs. */
-#  define ALL_OF(array, dim, index) index = lbound(array, dim), ubound(array, dim)
-#  define HAS_VAL(array, val) any(array == val)
-#  define IS_IOSTAT_BAD(ios) (is_iostat_eor(ios) .or. is_iostat_end(ios))
-#  define IS_IOSTAT_OK(ios) (.not.IS_IOSTAT_BAD(ios))
+#  define quote_left(x) "x
+#  define quote_right(x) x"
+#  define quote(x) quote_right(quote_left(x))
+#  define warn(message) write(ERROR_UNIT, *) "WARN: ", WHERE_AM_I, (message)
+#  define raise(message) write(ERROR_UNIT, *) "RAISE: ", WHERE_AM_I, (message); stop 1
+#  define ALL_OF(array, dim, index) index = lbound((array), (dim)), ubound((array), (dim))
+#  define has_val(array, val) (any((array) == (val)))
+#  define is_iostat_bad(ios) (is_iostat_eor(ios) .or. is_iostat_end(ios))
+#  define is_iostat_ok(ios) (.not.is_iostat_bad(ios))
 #  define I (0, 1)
 
-#  define WARN_IF(isBad, ...) \
+#  define warn_if(isBad) \
      if(isBad)then; \
-       WARN(#isBad, " ", ##__VA_ARGS__); \
+       warn(quote(isBad)); \
      end if
 
-#  define RAISE_IF(isBad, ...) \
+#  define raise_if(isBad) \
      if(isBad)then; \
-       RAISE(#isBad, " ", ##__VA_ARGS__); \
+       raise(quote(isBad)); \
      end if
-#  define ASSERT(isOk, ...) RAISE_IF(.not.(isOk), ##__VA_ARGS__)
-#  define TEST(isPass, ...)\
-     ASSERT(isPass, ##__VA_ARGS__);\
+#  define assert(isOk) raise_if(.not.(isOk))
+#  define test(isOk) \
+     assert(isOk); \
      write(OUTPUT_UNIT, '(a)', advance = 'no') '.'
 
 #  ifdef DEBUG
-#    define DEBUG_RAISE_IF(isBad, ...) RAISE_IF(isBad, ##__VA_ARGS__)
-#    define DEBUG_PRINT(...) write(ERROR_UNIT, *) "DEBUG: ", WHERE_AM_I, ##__VA_ARGS__
-#    define DEBUG_PRINT_VARIABLE(x) write(ERROR_UNIT, *) "DEBUG: ", WHERE_AM_I, #x, x /* Print a variable name and it's value. */
+#    define debug_raise_if(isBad) raise_if(isBad)
+#    define debug_print(x) write(ERROR_UNIT, *) "DEBUG: ", WHERE_AM_I, (x)
+#    define debug_print_variable(x) write(ERROR_UNIT, *) "DEBUG: ", WHERE_AM_I, quote(x), (x)
 #  else
-#    define DEBUG_PRINT(...)
-#    define DEBUG_PRINT_VARIABLE(x)
-#    define DEBUG_RAISE_IF(isBad, ...)
+#    define debug_raise_if(isBad)
+#    define debug_print(x)
+#    define debug_print_variable(x)
 #  endif
-#  define DEBUG_ASSERT(isOK, ...) DEBUG_RAISE_IF(.not.(isOk), ##__VA_ARGS__)
+#  define debug_assert(isOK) debug_raise_if(.not.(isOk))
 #endif
