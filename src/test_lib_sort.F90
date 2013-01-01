f21fa818ced00bd14572ffde41ab7fb3cca39391
kshramt <thisisdummy@example.com>
2013-01-01 21:41:15 +0900

    Delete `test_lib_sort.f90'

--- src/test_lib_sort.F90
+++
@@ -1,6 +0,0 @@
-program test_lib_sort
-  use lib_sort, only: qsort
-  implicit none
-  print*, qsort(huge(0.)*(/rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand()/))
-  stop
-end program test_lib_sort
