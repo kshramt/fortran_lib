fdf197221aa2e8117296360dd0087856009a643e
kshramt <thisisdummy@example.com>
2013-01-05 10:34:00 +0900

    Add test for stack of 2D array

--- src/lib_stack_test.F90
+++ src/lib_stack_test.F90
@@ -2,15 +2,17 @@
 program lib_stack_test
   USE_UTILS_H
   use, intrinsic:: iso_fortran_env, only:OUTPUT_UNIT
-  use lib_stack, only: IntegerDim0KindINT32Stack, IntegerDim1KindINT32Stack
+  use lib_stack, only: IntegerDim0KindINT32Stack, IntegerDim1KindINT32Stack, IntegerDim2KindINT32Stack
   use lib_stack, only: push, pop
 
   implicit none
 
   type(IntegerDim0KindINT32Stack):: stack0
   type(IntegerDim1KindINT32Stack):: stack1
+  type(IntegerDim2KindINT32Stack):: stack2
   Integer:: val0
   Integer, allocatable:: val1(:)
+  Integer, allocatable:: val2(:, :)
   Logical:: isSuccess
 
   ! dim = 0
@@ -68,6 +70,16 @@ program lib_stack_test
   test(.not.isSuccess)
   isSuccess = pop(stack1, val1)
   test(.not.isSuccess)
+
+  ! dim 2
+  call push(stack2, reshape([1, 2, 3, 4, 5, 6], [2, 3]))
+  call push(stack2, reshape([1, 2, 3, 4, 5, 6, 7, 8], [4, 2]))
+  isSuccess = pop(stack2, val2)
+  test(isSuccess)
+  test(all(shape(val2) == [4, 2]))
+  isSuccess = pop(stack2, val2)
+  test(isSuccess)
+  test(all(shape(val2) == [2, 3]))
 
   write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
 
