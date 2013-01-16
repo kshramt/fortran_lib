06aa862f067b4fce0b2f97bc37124b9754c9cc41
kshramt <thisisdummy@example.com>
2013-01-16 19:10:35 +0900

    Fix `kind_from_type'

--- src/lib_reflectable_test.F90
+++ src/lib_reflectable_test.F90
@@ -1,13 +1,14 @@
 #include "utils.h"
 program lib_reflectable_test
-  use, intrinsic:: iso_fortran_env, only: INPUT_UNIT, OUTPUT_UNIT, ERROR_UNIT
+  USE_UTILS_H
+  use, intrinsic:: iso_fortran_env, only: INPUT_UNIT, OUTPUT_UNIT, ERROR_UNIT, REAL32
   use lib_reflectable
 
   implicit none
   
   test(stem_from_type(0.0) == 'Real')
   test(dim_from_type(0.0) == 0)
-  test(kind_from_type(0.0) == 'REAL32')
+  test(kind_from_type(0.0) == REAL32)
   test(str_from_type(0.0) == 'RealDim0KindREAL32')
 
   write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
