0ab6043953fb71c0a83950c9c70d078ef9284db4
kshramt <thisisdummy@example.com>
2013-01-16 19:02:38 +0900

    Array index should be INT64

--- src/utils.h
+++ src/utils.h
@@ -13,7 +13,7 @@
 #  endif
 #  define warn(message) write(ERROR_UNIT, *) "WARN: ", WHERE_AM_I, (message)
 #  define raise(message) write(ERROR_UNIT, *) "RAISE: ", WHERE_AM_I, (message); stop 1
-#  define ALL_OF(array, dim, index) index = lbound(array, dim), ubound(array, dim)
+#  define ALL_OF(array, dim_, index) index = lbound(array, dim = dim_, kind = INT64), ubound(array, dim = dim_, kind = INT64)
 #  define has_val(array, val) (any((array) == (val)))
 #  define is_iostat_ok(ios) (ios == 0)
 #  define is_iostat_bad(ios) (.not.is_iostat_ok(ios))
