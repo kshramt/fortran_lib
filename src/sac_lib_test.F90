931cec8012555f650aab02826f40fd1f20d0d8a5
kshramt <thisisdummy@example.com>
2013-04-24 16:29:21 +0900

    Rename: w1 -> wHead, w2 -> wBody or wIo

--- src/sac_lib_test.F90
+++ src/sac_lib_test.F90
@@ -6,77 +6,77 @@ program sac_lib_test
 
   implicit none
 
-  type(Sac):: w1, w2
+  type(Sac):: wHead, wBody, wIo
   Real(kind = SAC_REAL_KIND), allocatable:: body(:)
   Logical:: isRaised
 
   ! = head
   ! == float
-  TEST(.not.is_delta_defined(w1))
-  call set_delta(w1, 0.1)
-  TEST(is_delta_defined(w1))
-  TEST(get_delta(w1) == 0.1)
-  call set_delta(w1, FLOAT_UNDEFINED)
-  TEST(.not.is_delta_defined(w1))
+  TEST(.not.is_delta_defined(wHead))
+  call set_delta(wHead, 0.1)
+  TEST(is_delta_defined(wHead))
+  TEST(get_delta(wHead) == 0.1)
+  call set_delta(wHead, FLOAT_UNDEFINED)
+  TEST(.not.is_delta_defined(wHead))
   ! == integer
-  TEST(.not.is_npts_defined(w1))
-  call set_npts(w1, 42)
-  TEST(is_npts_defined(w1))
-  TEST(get_npts(w1) == 42)
-  call set_npts(w1, INTEGER_UNDEFINED)
-  TEST(.not.is_npts_defined(w1))
+  TEST(.not.is_npts_defined(wHead))
+  call set_npts(wHead, 42)
+  TEST(is_npts_defined(wHead))
+  TEST(get_npts(wHead) == 42)
+  call set_npts(wHead, INTEGER_UNDEFINED)
+  TEST(.not.is_npts_defined(wHead))
   ! == enumerated
-  TEST(.not.is_iftype_defined(w1))
-  call set_iftype(w1, 'itime')
-  TEST(is_iftype_defined(w1))
-  TEST(get_iftype(w1) == 'itime')
-  call set_iftype(w1, 'anything other than enumerated values', isRaised)
+  TEST(.not.is_iftype_defined(wHead))
+  call set_iftype(wHead, 'itime')
+  TEST(is_iftype_defined(wHead))
+  TEST(get_iftype(wHead) == 'itime')
+  call set_iftype(wHead, 'anything other than enumerated values', isRaised)
   TEST(isRaised)
   ! == logical
-  call set_leven(w1, .true.)
-  TEST(is_leven_defined(w1))
-  TEST(get_leven(w1))
-  call set_leven(w1, .false.)
-  TEST(is_leven_defined(w1))
-  TEST(.not.get_leven(w1))
+  call set_leven(wHead, .true.)
+  TEST(is_leven_defined(wHead))
+  TEST(get_leven(wHead))
+  call set_leven(wHead, .false.)
+  TEST(is_leven_defined(wHead))
+  TEST(.not.get_leven(wHead))
   ! == long string
-  TEST(.not.is_kstnm_defined(w1))
-  call set_kstnm(w1, 'JMA/ERM')
-  TEST(is_kstnm_defined(w1))
-  TEST(get_kstnm(w1) == 'JMA/ERM')
-  call set_kstnm(w1, STRING_UNDEFINED)
-  TEST(.not.is_kstnm_defined(w1))
-  call set_kstnm(w1, '0123456789abcdef')
-  TEST(get_kstnm(w1) == '0123456789abcdef')
-  call set_kstnm(w1, '0123456789abcdefg', isRaised)
+  TEST(.not.is_kstnm_defined(wHead))
+  call set_kstnm(wHead, 'JMA/ERM')
+  TEST(is_kstnm_defined(wHead))
+  TEST(get_kstnm(wHead) == 'JMA/ERM')
+  call set_kstnm(wHead, STRING_UNDEFINED)
+  TEST(.not.is_kstnm_defined(wHead))
+  call set_kstnm(wHead, '0123456789abcdef')
+  TEST(get_kstnm(wHead) == '0123456789abcdef')
+  call set_kstnm(wHead, '0123456789abcdefg', isRaised)
   TEST(isRaised)
   ! == short string
-  TEST(.not.is_kevnm_defined(w1))
-  call set_kevnm(w1, 'japan')
-  TEST(is_kevnm_defined(w1))
-  TEST(get_kevnm(w1) == 'japan')
-  call set_kevnm(w1, STRING_UNDEFINED)
-  TEST(.not.is_kevnm_defined(w1))
-  call set_kevnm(w1, '12345678')
-  TEST(get_kevnm(w1) == '12345678')
-  call set_kevnm(w1, '123456789', isRaised)
+  TEST(.not.is_kevnm_defined(wHead))
+  call set_kevnm(wHead, 'japan')
+  TEST(is_kevnm_defined(wHead))
+  TEST(get_kevnm(wHead) == 'japan')
+  call set_kevnm(wHead, STRING_UNDEFINED)
+  TEST(.not.is_kevnm_defined(wHead))
+  call set_kevnm(wHead, '12345678')
+  TEST(get_kevnm(wHead) == '12345678')
+  call set_kevnm(wHead, '123456789', isRaised)
   TEST(isRaised)
   ! = body
   ! == itime
-  ! call get_body(w1, body, isRaised)
+  ! call get_body(wBody, body, isRaised)
   ! == ixy
   ! == iamph
   ! == irlim
   ! == ixyz
   ! = read
   ! = write
-  call set_iftype(w2, 'itime')   ! required
-  call set_delta(w2, 0.05)   ! required
+  call set_iftype(wIo, 'itime')   ! required
+  call set_delta(wIo, 0.05)   ! required
 
-  call set_body(w2, real([1, 2, 3, 4, 5, 6, 7])) ! npts should be larger than 5 (SAC's limitation).
-  call write(w2, 'tmp.sac')
-  call read(w2, 'tmp.sac')
-  call get_body(w2, body)
+  call set_body(wIo, real([1, 2, 3, 4, 5, 6, 7])) ! npts should be larger than 5 (SAC's limitation).
+  call write(wIo, 'tmp.sac')
+  call read(wIo, 'tmp.sac')
+  call get_body(wIo, body)
   print*, body
 
   write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
