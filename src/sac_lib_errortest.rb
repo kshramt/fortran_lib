0c12ed40b6f8bdfba4c4e31d89aef86e55ce803d
kshramt <thisisdummy@example.com>
2013-05-15 21:17:28 +0900

    Split 'sac_lib_errortest.rb' into general part and test codes

--- src/sac_lib_errortest.rb
+++ src/sac_lib_errortest.rb
@@ -1,28 +1,3 @@
-require 'fileutils'
-
-DIR = File.basename(File.expand_path(__FILE__), '.rb')
-$dependencies = []
-$compiler = ''
-$setup_code = ''
-$case_codes = {}
-$teardown_code = ''
-
-def compile(code)
-  $compile_code = code
-end
-
-def setup(code)
-  $setup_code = code
-end
-
-def errortest(title, code)
-  $case_codes[title] = code
-end
-
-def teardown(code)
-  $teardown_code = code
-end
-
 $dependencies << 'sac_lib.o'
 $compiler = ENV['MY_FORTRAN']
 
@@ -30,48 +5,23 @@ setup <<-EOS
 # include "../utils.h"
 program runner
   USE_UTILS_H
-  use, non_intrinsic:: sac_lib, only: get_iftype, Sac
+  use, non_intrinsic:: sac_lib
 
   implicit none
 
-  type(Sac):: w1, w2
-  Character:: tmp
+  type(Sac):: wHead
+  Character:: cTrash
+  Integer:: iTrash
 EOS
 
-errortest 'iftype', <<-EOS
-  tmp = get_iftype(w1)
-EOS
+errortest 'set_kstnm with too long argument', "call set_kstnm(wHead, '0123456789abcdefg')"
+errortest 'set_kevnm with too long argument', "call set_kevnm(wHead, '123456789')"
+errortest 'set_iftype with invalid argument', "call set_iftype(wHead, 'anything other than enumerated values')"
+
+errortest 'get_iftype for undefined value', "cTrash = get_iftype(wHead)"
+errortest 'get_imagsrc for undefined value', "cTrash = get_imagsrc(wHead)"
 
 teardown <<-EOS
   stop
 end program runner
 EOS
-
-FileUtils.mkdir_p(DIR)
-$case_codes.each{|title, code|
-target = File.join(DIR, title) + '.F90'
-  if !File.readable?(target) || File.mtime(target) <= File.mtime(__FILE__)
-    File.write(target, $setup_code + code + $teardown_code)
-  end
-}
-
-makefile = DIR + '.make'
-open(makefile, 'w'){|io|
-  io.puts "all: " + $case_codes.keys.map{|title| File.join(DIR, title) + '.exe'}.join(' ')
-  $case_codes.each{|title, _|
-    base =  File.join(DIR, title)
-    io.puts "#{base}.exe: #{$dependencies.join(' ')} #{base}.F90"
-    io.puts "\t#{$compiler} -o $@ $^"
-  }
-}
-
-system "make -f #{makefile}"
-$case_codes.each{|title, _|
-  exe = File.join(DIR, title) + '.exe'
-  command = "#{exe}"
-  if system(command)
-    $stderr.put "FAIL: #{exe}"
-  else
-    puts 'PASS'
-  end
-}
