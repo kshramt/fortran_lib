5c0990d48e97285afb93c4caa8356ee4b76bcf75
kshramt <thisisdummy@example.com>
2013-05-17 22:02:03 +0900

    Fix lib_io_test.F90

--- src/lib_io_test.F90
+++ src/lib_io_test.F90
@@ -5,21 +5,24 @@ program lib_io_test
     REAL64, INT8
   use lib_io
   use:: lib_comparable, only: equivalent
+  use:: lib_character, only: operator(+)
   
   implicit none
   
   Real(kind = REAL64), allocatable:: xs(:), xsOriginal(:)
-  Integer(kind = INT8), allocatable:: ns(:), nsOriginal(:)
+  Integer(kind = INT8), allocatable:: ns(:, :), nsOriginal(:, :)
 
-  ! read_array, write_array
+  ! = read_array, write_array, number_of_lines
+  ! == Real 1D
   xs = [1, 2, 3, 4, 5]
   xsOriginal = xs
-  call write_array('xs.array', xs)
+  call write_array('xs.array', xs, 'Real 1D test', 'Multiple descriptions.')
   TEST(all(equivalent(xs, xsOriginal)))
   call read_array('xs.array', xs)
   TEST(all(equivalent(xs, xsOriginal)))
 
-  ns = int([1, 2, 3, 4, 5], kind = kind(ns))
+  ! == Integer 2D
+  ns = reshape(int([1, 2, 3, 4, 5, 6], kind = kind(ns)), [2, 3])
   nsOriginal = ns
   call write_array('ns.array', ns)
   TEST(all(equivalent(ns, nsOriginal)))
