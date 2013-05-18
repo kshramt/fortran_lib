5caa86e390c0446dc5428bd16517579d27c19d0b
kshramt <thisisdummy@example.com>
2013-05-19 08:13:05 +0900

    Delete DEG_FROM_RAD and RAD_FROM_DEG; Add deg_from_rad() and rad_from_deg()

--- src/constant_lib_test.F90
+++ src/constant_lib_test.F90
@@ -11,8 +11,8 @@ program constant_lib_test
    TEST(get_nan() /= get_nan())
    TEST(get_infinity() > huge(0.0_REAL128))
    TEST(equivalent(PI_REAL64,           3.141592653589793_REAL64))
-   TEST(equivalent(RAD_FROM_DEG_REAL64, 0.017453292519943295_REAL64))
-   TEST(equivalent(DEG_FROM_RAD_REAL64, 57.29577951308232_REAL64))
+   TEST(equivalent(rad_from_deg(1.0_REAL64), 0.017453292519943295_REAL64))
+   TEST(equivalent(deg_from_rad(1.0_REAL64), 57.29577951308232_REAL64))
 
    write(OUTPUT_UNIT, *) "SUCCESS: ", __FILE__
    stop
