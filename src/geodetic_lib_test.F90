eee584ba630bfd34ab8d7401ce5637bade0ada19
kshramt <thisisdummy@example.com>
2013-05-20 01:54:32 +0900

    Rename ONE -> ONE128, ZERO -> ZERO128 and lonLatH -> lonLatH128

--- src/geodetic_lib_test.F90
+++ src/geodetic_lib_test.F90
@@ -6,41 +6,40 @@ program geodetic_lib_test
    use, non_intrinsic:: geodetic_lib
 
    implicit none
-   Real(kind = REAL128):: lonLatH(1:3)
-   Real(kind = kind(lonLatH)), parameter:: ZERO = 0, ONE = 1
+   Real(kind = REAL128):: lonLatH128(1:3)
+   Real(kind = kind(lonLatH128)), parameter:: ZERO128 = 0, ONE128 = 1
 
-   lonLatH = wgs84_from_ecef(ONE, ZERO, ZERO)
-   TEST(lonLatH(1) == 0)
-   TEST(lonLatH(2) == 0)
-   lonLatH = wgs84_from_ecef(-ONE, ZERO, ZERO)
-   TEST(lonLatH(1) == 180)
-   TEST(lonLatH(2) == 0)
-   lonLatH = wgs84_from_ecef(ONE, ONE, ZERO)
-   TEST(lonLatH(1) == 45)
-   TEST(lonLatH(2) == 0)
-   lonLatH = wgs84_from_ecef(ONE, -ONE, ZERO)
-   TEST(lonLatH(1) == -45)
-   TEST(lonLatH(2) == 0)
-   lonLatH = wgs84_from_ecef(ONE, ZERO, ONE)
-   TEST(lonLatH(1) == 0)
-   TEST(lonLatH(2) > 0)
-   lonLatH = wgs84_from_ecef(ONE, ZERO, -ONE)
-   TEST(lonLatH(1) == 0)
-   TEST(lonLatH(2) < 0)
+   lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, ZERO128)
+   TEST(lonLatH128(1) == 0)
+   TEST(lonLatH128(2) == 0)
+   lonLatH128 = wgs84_from_ecef(-ONE128, ZERO128, ZERO128)
+   TEST(lonLatH128(1) == 180)
+   TEST(lonLatH128(2) == 0)
+   lonLatH128 = wgs84_from_ecef(ONE128, ONE128, ZERO128)
+   TEST(lonLatH128(1) == 45)
+   TEST(lonLatH128(2) == 0)
+   lonLatH128 = wgs84_from_ecef(ONE128, -ONE128, ZERO128)
+   TEST(lonLatH128(1) == -45)
+   TEST(lonLatH128(2) == 0)
+   lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, ONE128)
+   TEST(lonLatH128(1) == 0)
+   TEST(lonLatH128(2) > 0)
+   lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, -ONE128)
+   TEST(lonLatH128(1) == 0)
+   TEST(lonLatH128(2) < 0)
+   lonLatH128 = wgs84_from_ecef(0.1e-13_REAL128, 0.0_REAL128, 1000.0_REAL128)
+   TEST(lonLatH128(2) < 90) ! DEBUG
+   TEST(lonLatH128(2) > real(89, kind = kind(lonLatH128)))
 
-   lonLatH = wgs84_from_ecef(0.1e-14_REAL128, 0.0_REAL128, 1000.0_REAL128)
-   TEST(lonLatH(2) < 90) ! DEBUG
-   TEST(lonLatH(2) > real(89, kind = kind(lonLatH)))
+   lonLatH128 = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, 1000.0_REAL128)
+   TEST(lonLatH128(2) == 90)
 
-   lonLatH = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, 1000.0_REAL128)
-   TEST(lonLatH(2) == 90)
+   lonLatH128 = wgs84_from_ecef(0.1e-14_REAL128, 0.0_REAL128, -1000.0_REAL128)
+   TEST(lonLatH128(2) > -90) ! DEBUG
+   TEST(lonLatH128(2) < real(-89, kind = kind(lonLatH128)))
 
-   lonLatH = wgs84_from_ecef(0.1e-14_REAL128, 0.0_REAL128, -1000.0_REAL128)
-   TEST(lonLatH(2) > -90) ! DEBUG
-   TEST(lonLatH(2) < real(-89, kind = kind(lonLatH)))
-
-   lonLatH = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, -1000.0_REAL128)
-   TEST(lonLatH(2) == -90)
+   lonLatH128 = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, -1000.0_REAL128)
+   TEST(lonLatH128(2) == -90)
 
    write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
 
