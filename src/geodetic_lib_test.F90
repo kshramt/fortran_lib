f82b1f5f31cd751cf0da17c337ddd487af1de21c
kshramt <thisisdummy@example.com>
2013-05-20 04:27:38 +0900

    Select a stable cost function according to (xy, z)
    Previous cost function was very sensitive to small deviation of lat around PI/2.
    This commit introduces two cost functions, one is suitable for lat value around PI/2, the other is suitable around 0.

--- src/geodetic_lib_test.F90
+++ src/geodetic_lib_test.F90
@@ -6,53 +6,48 @@ program geodetic_lib_test
    use, non_intrinsic:: geodetic_lib
 
    implicit none
-   Real(kind = REAL64):: lonLatH64(1:3)
+   Real(kind = REAL64):: xyz64(1:3)
    Real(kind = REAL128):: lonLatH128(1:3)
-   Real(kind = kind(lonLatH64)), parameter:: ZERO64 = 0, ONE64 = 1
+   Real(kind = kind(xyz64)), parameter:: ZERO64 = 0, ONE64 = 1
    Real(kind = kind(lonLatH128)), parameter:: ZERO128 = 0, ONE128 = 1
 
-   lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, ZERO128)
+   ! ecef_from_wgs84
+   xyz64 = ecef_from_wgs84(0.0_REAL64, 0.0_REAL64, 0.0_REAL64)
+   TEST(equivalent(xyz64(1), WGS84_A))
+   TEST(equivalent(xyz64(2), ZERO64))
+   TEST(equivalent(xyz64(3), ZERO64))
+
+   xyz64 = ecef_from_wgs84(0.0_REAL64, 90.0_REAL64, 0.0_REAL64)
+   TEST(abs(xyz64(1)) < 1e-7)
+   TEST(abs(xyz64(2)) < 1e-7)
+   TEST(equivalent(xyz64(3), WGS84_B))
+
+   ! wgs84_from_ecef
+   lonLatH128 = wgs84_from_ecef(ZERO128, ZERO128, ZERO128)
+   TEST(equivalent(lonLatH128(1), ZERO128))
+   TEST(lonLatH128(2) > 89)
+
+   lonLatH128 = wgs84_from_ecef(ONE128*6000000, ZERO128, ZERO128)
    TEST(lonLatH128(1) == 0)
    TEST(lonLatH128(2) == 0)
-   lonLatH128 = wgs84_from_ecef(-ONE128, ZERO128, ZERO128)
+   lonLatH128 = wgs84_from_ecef(-ONE128*6000000, ZERO128, ZERO128)
    TEST(lonLatH128(1) == 180)
    TEST(lonLatH128(2) == 0)
-   lonLatH128 = wgs84_from_ecef(ONE128, ONE128, ZERO128)
+   lonLatH128 = wgs84_from_ecef(ONE128*6000000, ONE128*6000000, ZERO128)
    TEST(lonLatH128(1) == 45)
    TEST(lonLatH128(2) == 0)
-   lonLatH128 = wgs84_from_ecef(ONE128, -ONE128, ZERO128)
+   lonLatH128 = wgs84_from_ecef(ONE128*6000000, -ONE128*6000000, ZERO128)
    TEST(lonLatH128(1) == -45)
    TEST(lonLatH128(2) == 0)
    lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, ONE128)
    TEST(lonLatH128(1) == 0)
-   TEST(lonLatH128(2) > 0)
+   TEST(lonLatH128(2) >= 0)
    lonLatH128 = wgs84_from_ecef(ONE128, ZERO128, -ONE128)
    TEST(lonLatH128(1) == 0)
-   TEST(lonLatH128(2) < 0)
+   TEST(lonLatH128(2) <= 0)
 
-   lonLatH64 = ecef_from_wgs84(0.0_REAL64, 0.0_REAL64, 0.0_REAL64)
-   TEST(equivalent(lonLatH64(1), WGS84_A))
-   TEST(equivalent(lonLatH64(2), ZERO64))
-   TEST(equivalent(lonLatH64(3), ZERO64))
-
-   lonLatH64 = ecef_from_wgs84(0.0_REAL64, 90.0_REAL64, 0.0_REAL64)
-   TEST(abs(lonLatH64(1)) < 1e-7)
-   TEST(abs(lonLatH64(2)) < 1e-7)
-   TEST(equivalent(lonLatH64(3), WGS84_B))
-
-   lonLatH128 = wgs84_from_ecef(0.1e-13_REAL128, 0.0_REAL128, 1000.0_REAL128)
-   TEST(lonLatH128(2) < 90) ! DEBUG
-   TEST(lonLatH128(2) > real(89, kind = kind(lonLatH128)))
-
-   lonLatH128 = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, 1000.0_REAL128)
-   TEST(lonLatH128(2) == 90)
-
-   lonLatH128 = wgs84_from_ecef(0.1e-14_REAL128, 0.0_REAL128, -1000.0_REAL128)
-   TEST(lonLatH128(2) > -90) ! DEBUG
-   TEST(lonLatH128(2) < real(-89, kind = kind(lonLatH128)))
-
-   lonLatH128 = wgs84_from_ecef(0.1e-15_REAL128, 0.0_REAL128, -1000.0_REAL128)
-   TEST(lonLatH128(2) == -90)
+   lonLatH128 = wgs84_from_ecef(epsilon(ONE128), ZERO128, ZERO128)
+   TEST(lonLatH128(2) > 89)
 
    write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
 
