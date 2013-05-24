961bc8a09d128d2b58746b14104e9c229d8dcedd
kshramt <thisisdummy@example.com>
2013-05-25 08:49:30 +0900

    Add default compiler

--- src/binary_tree_map_lib_errortest.rb
+++ src/binary_tree_map_lib_errortest.rb
@@ -1,5 +1,5 @@
 $dependencies << 'binary_tree_map_lib.o'
-$compiler = ENV['MY_FORTRAN_DEBUG']
+$compiler = ENV.fetch('MY_FORTRAN_DEBUG', 'gfortran -ffree-line-length-none -fmax-identifier-length=63 -pipe -cpp -C -Wall -fbounds-check -O0 -fbacktrace -ggdb -pg -DDEBUG')
 
 setup <<-EOS
 # include "../utils.h"
