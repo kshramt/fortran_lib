af791830e5e6ea6a7dc81ace1d1acfb36615c00e
kshramt <thisisdummy@example.com>
2014-03-18 05:13:11 +0900

    Add `array_lib/iota`

---
+++ src/array_lib_test.F90
@@ -0,0 +1,34 @@
+#include "utils.h"
+program main
+   USE_UTILS_H
+   use, intrinsic:: iso_fortran_env, only: INPUT_UNIT, OUTPUT_UNIT, ERROR_UNIT
+   use, intrinsic:: iso_fortran_env, only: INT64
+   use, non_intrinsic:: array_lib, only: iota
+   
+   implicit none
+
+   Integer:: i
+
+   TEST(all(iota(0) == [(i, i = 1, 0)]))
+   TEST(all(iota(1) == [1]))
+   TEST(all(iota(2) == [1, 2]))
+   TEST(all(iota(3) == [1, 2, 3]))
+
+   TEST(all(iota(-3, -4) == [(i, i = 1, 0)]))
+   TEST(all(iota(-3, -3) == [-3]))
+   TEST(all(iota(-3, -2) == [-3, -2]))
+   TEST(all(iota(-3, -1) == [-3, -2, -1]))
+
+   TEST(all(iota(2, 3, -3) == [(i, i = 1, 0)]))
+   TEST(all(iota(2, 2, -3) == [2]))
+   TEST(all(iota(2, 1, -3) == [2]))
+   TEST(all(iota(2, 0, -3) == [2]))
+   TEST(all(iota(2, -1, -3) == [2, -1]))
+   TEST(all(iota(2, -2, -3) == [2, -1]))
+   TEST(all(iota(2, -3, -3) == [2, -1]))
+   TEST(all(iota(2, -4, -3) == [2, -1, -4]))
+
+   write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
+   
+   stop
+end program main
