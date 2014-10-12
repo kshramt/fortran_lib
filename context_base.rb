7de75413a16faa4b87defba5c57739b8085fca44
kshramt <thisisdummy@example.com>
2014-10-13 02:54:37 +0900

    Add `memoized_read` to make recursive ERB loading faster

--- context_base.rb
+++ context_base.rb
@@ -5,3 +5,10 @@ class ContextBase
     ERB.new(template).result(binding)
   end
 end
+
+
+$memoized_read_table = {}
+def memoized_read(path)
+  _path = File.expand_path(path)
+  $memoized_read_table[_path] or $memoized_read_table[_path] = open(_path).read
+end
