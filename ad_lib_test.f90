cd3b783bd7857e9efd138312a035c75e758220d4
kshramt <thisisdummy@example.com>
2015-05-23 05:12:11 +0900

    Test `**` and fix a bug

--- ad_lib_test.f90
+++ ad_lib_test.f90
@@ -1,15 +1,15 @@
 #include "fortran_lib.h"
 program main
    USE_FORTRAN_LIB_H
-   use, intrinsic:: iso_fortran_env, only: INPUT_UNIT, OUTPUT_UNIT, ERROR_UNIT
+   use, intrinsic:: iso_fortran_env, only: INPUT_UNIT, OUTPUT_UNIT, ERROR_UNIT, real64
    use, non_intrinsic:: comparable_lib, only: almost_equal
    use, non_intrinsic:: ad_lib
 
    implicit none
 
    type(Dual64_2_2):: x, y, z
-   Real, parameter:: a = 6, b = 2, zero = 0
-   Real:: h(2, 2)
+   Real(kind=real64), parameter:: a = 6, b = 2, zero = 0
+   Real(kind=real64):: h(2, 2), j(2)
 
    x%f = a
    x%g(1) = 1
@@ -70,8 +70,23 @@ program main
 
    z = x**y
    TEST(real(z) == a**b)
+   TEST(all(almost_equal(jaco(z), [b*a**(b - 1), log(a)*a**b])))
+   h = hess(z)
+   TEST(almost_equal(h(1, 1), b*(b - 1)*a**(b - 2)))
+   TEST(almost_equal(h(1, 2), a**(b - 1) + b*log(a)*a**(b - 1)))
+   TEST(almost_equal(h(2, 1), a**(b - 1) + b*log(a)*a**(b - 1)))
+   TEST(almost_equal(h(2, 2), log(a)**2*a**b))
 
    z = x**x
+   TEST(real(z) == a**a)
+   j = jaco(z)
+   TEST(almost_equal(j(1), (log(a) + 1)*a**a))
+   TEST(j(2) == 0)
+   h = hess(z)
+   TEST(almost_equal(h(1, 1), a**(a - 1) + (log(a) + 1)**2*a**a))
+   TEST(h(1, 2) == 0)
+   TEST(h(2, 2) == 0)
+   TEST(h(2, 2) == 0)
 
    write(OUTPUT_UNIT, *) 'SUCCESS: ', __FILE__
 
