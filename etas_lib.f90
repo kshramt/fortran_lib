0ad099ded9757729d7e9f8bda07efb83b2cc375c
kshramt <thisisdummy@example.com>
2015-05-24 15:33:06 +0900

    Pairwise summation

--- etas_lib.f90
+++ etas_lib.f90
@@ -22,22 +22,47 @@ module etas_lib
 contains
 
    function log_likelihood_etas(t_end, normalize_interval, c, p, alpha, k1, mu, ts, ms) result(ret)
+      Integer(kind=int64):: one = 1
       Real(kind=real64):: ret
       Real(kind=kind(ret)), intent(in):: t_end, normalize_interval, c, p, alpha, k1, mu, ts(:), ms(:)
-      Real(kind=kind(ret)), allocatable:: ks(:)
+      Real(kind=kind(ret)):: ks(size(ms))
       Real(kind=kind(ret)):: mu_normalized
-      Integer(kind=int64):: n, i
+      Integer(kind=kind(one)):: n
 
       n = size(ts, kind=kind(n))
       ASSERT(size(ms, kind=kind(n)) == n)
       ks = kernel_coeff(ms, c, p, alpha, k1, normalize_interval)
       mu_normalized = mu/normalize_interval
-      ret = -lambda_integrate(t_end, c, p, mu_normalized, ts, ks)
-      !$omp parallel do reduction(+:ret) schedule(dynamic)
-      do i = 1, n
-         ret = ret + log(lambda(i, c, p, mu_normalized, ts, ks))
-      end do
+      ret = sum_log_lambda(one, n, c, p, mu_normalized, ts, ks) - lambda_integrate(t_end, c, p, mu_normalized, ts, ks)
    end function log_likelihood_etas
+
+
+   recursive function sum_log_lambda(i1, i2, c, p, mu_normalized, ts, ks) result(ret)
+      Integer(kind=int64), parameter:: blocksize = 2**2
+      Real(kind=real64):: ret, ret_
+      Integer(kind=kind(blocksize)), intent(in):: i1, i2
+      Real(kind=kind(ret)), intent(in):: c, p, mu_normalized, ts(:), ks(:)
+      Integer(kind=int64):: i, i_mid
+
+      if(i1 + blocksize > i2)then
+         ret = log(lambda(i1, c, p, mu_normalized, ts, ks))
+         do i = i1 + 1, i2
+            ret = ret + log(lambda(i, c, p, mu_normalized, ts, ks))
+         end do
+      else
+         i_mid = (i1 + i2)/2
+         !$omp parallel
+         !$omp single
+         !$omp task shared(ret_) untied
+         ret_ = sum_log_lambda(i1, i_mid, c, p, mu_normalized, ts, ks)
+         !$omp end task
+         ret = sum_log_lambda(i_mid + 1, i2, c, p, mu_normalized, ts, ks)
+         !$omp taskwait
+         !$omp end single
+         !$omp end parallel
+         ret = ret + ret_
+      end if
+   end function sum_log_lambda
 
 
    pure function lambda_i(i, c, p, mu_normalized, ts, ks) result(ret)
