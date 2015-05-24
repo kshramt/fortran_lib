548ff3c8c6487fd84ae1c6a57e0bab3f50d752bb
kshramt <thisisdummy@example.com>
2015-05-24 23:36:12 +0900

    Fix

--- etas_solve.f90
+++ etas_solve.f90
@@ -2,16 +2,17 @@ program main
    use, intrinsic:: iso_fortran_env, only: input_unit, output_unit, error_unit, real64, int64
    use, non_intrinsic:: comparable_lib, only: almost_equal
    use, non_intrinsic:: optimize_lib, only: NewtonStateRealDim0KindREAL64, init, update
-   use, non_intrinsic:: ad_lib, only: Dual64_2_5, real, hess, jaco, operator(-)
+   use, non_intrinsic:: ad_lib, only: Dual64_2_5, real, hess, jaco, operator(-), exp
    use, non_intrinsic:: etas_lib, only: log_likelihood_etas
 
    implicit none
 
+   Real(kind=real64), parameter:: c_min = 1d-6
    Real(kind=real64), allocatable:: ts(:), ms(:)
    type(NewtonStateRealDim0KindREAL64):: s
    Real(kind=real64):: c_p_alpha_k1_mu_best(5)
    Real(kind=real64):: t_end, normalize_interval
-   Real(kind=real64):: f, g(5), H(5, 5), f_best
+   Real(kind=real64):: f, g(5), H(5, 5), f_best, bound
    type(Dual64_2_5):: c, p, alpha, k1, mu, fgh
    Integer(kind=int64):: n, i
    Logical:: converge
@@ -32,11 +33,16 @@ program main
 
    f_best = huge(f_best)
    do
-      c = Dual64_2_5(c_p_alpha_k1_mu_best(1), [1, 0, 0, 0, 0])
-      p = Dual64_2_5(c_p_alpha_k1_mu_best(2), [0, 1, 0, 0, 0])
-      alpha = Dual64_2_5(c_p_alpha_k1_mu_best(3), [0, 0, 1, 0, 0])
-      k1 = Dual64_2_5(c_p_alpha_k1_mu_best(4), [0, 0, 0, 1, 0])
-      mu = Dual64_2_5(c_p_alpha_k1_mu_best(5), [0, 0, 0, 0, 1])
+      ! if(s%x(1) <= c_min)then
+      !    bound = (c_min - s%x_prev(1))/(s%x(1) - s%x_prev(1))
+      !    s%x = bound*(s%x - s%x_prev) + s%x_prev
+      !    print*, bound
+      ! end if
+      c = Dual64_2_5(s%x(1), [1, 0, 0, 0, 0])
+      p = Dual64_2_5(s%x(2), [0, 1, 0, 0, 0])
+      alpha = Dual64_2_5(s%x(3), [0, 0, 1, 0, 0])
+      k1 = Dual64_2_5(s%x(4), [0, 0, 0, 1, 0])
+      mu = Dual64_2_5(s%x(5), [0, 0, 0, 0, 1])
       ! todo: implement negation
       fgh = 0 - log_likelihood_etas(t_end, normalize_interval, c, p, alpha, k1, mu, ts, ms)
       f = real(fgh)
@@ -44,14 +50,14 @@ program main
       H = hess(fgh)
       write(output_unit, *) s%is_convex, s%is_within, f, s%x, g
       call update(s, f, g, H, 'u')
-      converge = all(almost_equal(c_p_alpha_k1_mu_best, s%x, relative=1d-6, absolute=1d-6)) .or. norm2(g) < 1d-6
+      converge = s%is_convex .and. s%is_within .and. (all(almost_equal(c_p_alpha_k1_mu_best, s%x, relative=1d-6, absolute=1d-6)) .or. norm2(g) < 1d-6)
       if(f < f_best)then
          c_p_alpha_k1_mu_best = s%x
          f_best = f
       end if
       if(converge) exit
    end do
-   write(output_unit, *) s%iter, c_p_alpha_k1_mu_best
+   write(output_unit, *) s%iter, f_best, c_p_alpha_k1_mu_best
 
    stop
 end program main
