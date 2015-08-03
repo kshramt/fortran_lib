c2e76c1b406faefe2930151ef672acd031789e28
kshramt <thisisdummy@example.com>
2015-08-03 14:53:40 +0900

    `elemental`ize  some `geometry_lib` functions

--- geometry_lib_test.f90
+++ geometry_lib_test.f90
@@ -10,6 +10,7 @@ program main
    Real(kind=real64):: pi = 2*atan2(1d0, 0d0)
    Real(kind=real64):: d
    Real(kind=real64):: th, th1, th2
+   Real(kind=real64):: u1, u2
    Integer(kind=int64):: i, j
 
 
@@ -24,15 +25,23 @@ program main
    TEST(shape_of([0.0, 0.5, 2.0, 1.0], [0.0, 1.0, 0.0, 0.0]) == right_convex_polygon)
 
 
-   TEST(almost_equal(distance_2d([4d0, 3d0], [0d0, 25d0/3]), 5d0))
-   TEST(almost_equal(distance_2d([4d0, 3d0], [-3d0, 25d0/3 + 4]), 5d0))
-   TEST(almost_equal(distance_2d(-[4d0, 3d0], -[0d0, 25d0/3]), 5d0))
-   TEST(almost_equal(distance_2d(-[4d0, 3d0], -[-3d0, 25d0/3 + 4]), 5d0))
+   TEST(almost_equal(distance(4d0, 3d0, 0d0, 25d0/3), 5d0))
+   TEST(almost_equal(distance(4d0, 3d0, -3d0, 25d0/3 + 4), 5d0))
+   TEST(almost_equal(distance(-4d0, -3d0, -0d0, -25d0/3), 5d0))
+   TEST(almost_equal(distance(-4d0, -3d0, 3d0, -(25d0/3 + 4)), 5d0))
 
-   TEST(all(almost_equal(perpendicular_2d([4d0, 3d0], [0d0, 25d0/3]), [4d0, 3d0], rtol=2*epsilon(0d0))))
-   TEST(all(almost_equal(perpendicular_2d([4d0, 3d0], [-3d0, 25d0/3 + 4]), [4d0, 3d0], rtol=2*epsilon(0d0))))
-   TEST(all(almost_equal(perpendicular_2d(-[4d0, 3d0], -[0d0, 25d0/3]), -[4d0, 3d0], rtol=2*epsilon(0d0))))
-   TEST(all(almost_equal(perpendicular_2d(-[4d0, 3d0], -[-3d0, 25d0/3 + 4]), -[4d0, 3d0], rtol=2*epsilon(0d0))))
+   call perpendicular(4d0, 3d0, 0d0, 25d0/3, u1, u2)
+   TEST(almost_equal(u1, 4d0, rtol=2*epsilon(0d0)))
+   TEST(almost_equal(u2, 3d0, rtol=2*epsilon(0d0)))
+   call perpendicular(4d0, 3d0, -3d0, 25d0/3 + 4, u1, u2)
+   TEST(almost_equal(u1, 4d0, rtol=2*epsilon(0d0)))
+   TEST(almost_equal(u2, 3d0, rtol=2*epsilon(0d0)))
+   call perpendicular(-4d0, -3d0, -0d0, -25d0/3, u1, u2)
+   TEST(almost_equal(u1, -4d0, rtol=2*epsilon(0d0)))
+   TEST(almost_equal(u2, -3d0, rtol=2*epsilon(0d0)))
+   call perpendicular(-4d0, -3d0, 3d0, -(25d0/3 + 4), u1, u2)
+   TEST(almost_equal(u1, -4d0, rtol=2*epsilon(0d0)))
+   TEST(almost_equal(u2, -3d0, rtol=2*epsilon(0d0)))
 
    do i = 0, 16
       th1 = 2*i*pi/17
@@ -40,12 +49,12 @@ program main
          th2 = 2*j*pi/13
          th = th2 - th1
          if(abs(th) > pi) th = sign(2*pi - abs(th), -th)
-         TEST(almost_equal(angle_2d(2*[cos(th1), sin(th1)], 3*[cos(th2), sin(th2)]), th, atol=4*epsilon(0d0), rtol=4*epsilon(0d0)))
+         TEST(almost_equal(angle(2*cos(th1), 2*sin(th1), 3*cos(th2), 3*sin(th2)), th, atol=4*epsilon(0d0), rtol=4*epsilon(0d0)))
       end do
    end do
 
 
-   call distance_and_angles_2d([-2/sqrt(3d0), 2d0], [2*sqrt(3d0), 2d0], d, th1, th2)
+   call distance_and_angles(-2/sqrt(3d0), 2d0, 2*sqrt(3d0), 2d0, d, th1, th2)
    TEST(almost_equal(d, 2d0))
    TEST(almost_equal(th1, pi/6))
    TEST(almost_equal(th2, -pi/3))
