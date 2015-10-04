b2efb08d83af476ef7ff2d82e4900141d219b48b
kshramt <thisisdummy@example.com>
2015-10-04 18:45:38 +0900

    More tests for `qags` on with `Dual`

--- quadpack_lib_test.f90
+++ quadpack_lib_test.f90
@@ -11,8 +11,8 @@ program main
 
    Real(kind=real64), parameter:: pi = 2*atan2(1d0, 0d0)
    Integer(kind=int32):: err, n_eval
-   Real(kind=real64):: ret, abs_err
-   type(Dual64_2_2):: dual_ret, dual_abs_err
+   Real(kind=real64):: ret, abs_err, a, b, q
+   type(Dual64_2_1):: dual_ret, dual_abs_err
 
    call qags(my_sin, 0d0, pi, ret, n_eval=n_eval, err=err, abs_err=abs_err)
    TEST(err == 0)
@@ -21,16 +21,33 @@ program main
    call qags(dual_3sin, 0d0, pi, dual_ret, n_eval=n_eval, err=err, abs_err=dual_abs_err)
    TEST(err == 0)
    TEST(almost_equal(real(dual_ret), 6d0, atol=real(dual_abs_err)))
-   TEST(all(almost_equal(grad(dual_ret), 2d0, atol=grad(dual_abs_err))))
+   TEST(all(almost_equal(grad(dual_ret), 2d0, atol=abs(grad(dual_abs_err)))))
 
    call qags(inv_x, 1d0/2, 2d0, ret, n_eval=n_eval, err=err, abs_err=abs_err)
    TEST(err == 0)
    TEST(almost_equal(ret, log(4d0), atol=abs_err))
 
+   call qags(inv_x_args, 1d0/2, 2d0, [3d0], ret, n_eval=n_eval, err=err, abs_err=abs_err)
+   TEST(err == 0)
+   TEST(almost_equal(ret, 3*log(4d0), atol=abs_err))
+
+   a = 1d0/2
+   b = 2d0
+   q = 1
+   call qags(dual_inv_x_pow_q, a, b, [Dual64_2_1(q, [1])], dual_ret, n_eval=n_eval, err=err, abs_err=dual_abs_err)
+   TEST(err == 0)
+   TEST(almost_equal(real(dual_ret), log(b/a), atol=real(dual_abs_err)))
+   TEST(all(almost_equal(grad(dual_ret), -(b**(1 - q)*log(b)**2 - a**(1 - q)*log(a)**2)/2, atol=abs(grad(dual_abs_err)))))
+
+   q = 2
+   call qags(dual_inv_x_pow_q, a, b, [Dual64_2_1(q, [1])], dual_ret, n_eval=n_eval, err=err, abs_err=dual_abs_err)
+   TEST(err == 0)
+   TEST(almost_equal(real(dual_ret), (b**(1 - q) - a**(1 - q))/(1 - q), atol=real(dual_abs_err)))
+   TEST(all(almost_equal(grad(dual_ret), ((-b**(1 - q)*log(b) + a**(1 - q)*log(a))*(1 - q) + (b**(1 - q) - a**(1 - q)))/(1 - q)**2, atol=abs(grad(dual_abs_err)))))
+
    call qags(inv_x2, 1d0/1024, 1024d0, ret, n_eval=n_eval, err=err, abs_err=abs_err)
    TEST(err == 0)
    TEST(almost_equal(ret, (1048575d0/1024) - (10*sqrt(10d0)*atan2(476625*sqrt(5d0/2), 23296d0)), atol=abs_err))
-
 
    write(output_unit, *) 'SUCCESS: ', __FILE__
 
@@ -69,8 +86,17 @@ contains
 
    pure function dual_3sin(x) result(ret)
       Real(kind=real64), intent(in):: x
-      type(Dual64_2_2):: ret
+      type(Dual64_2_1):: ret
 
-      ret = sin(x)*Dual64_2_2(3, 1)
+      ret = sin(x)*Dual64_2_1(3, [1])
    end function dual_3sin
+
+   pure function dual_inv_x_pow_q(x, args) result(ret)
+      Real(kind=real64), intent(in):: x
+      ! this should NOT be args(1) to match the qags interface
+      type(Dual64_2_1), intent(in):: args(:)
+      type(Dual64_2_1):: ret
+
+      ret = 1/x**args(1)
+   end function dual_inv_x_pow_q
 end program main
