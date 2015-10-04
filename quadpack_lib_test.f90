ffdd02fe1bbe40bbb5854cad2d490db783fa086d
kshramt <thisisdummy@example.com>
2015-10-04 13:51:58 +0900

    Basic tests for `Dual`

--- quadpack_lib_test.f90
+++ quadpack_lib_test.f90
@@ -4,18 +4,24 @@ program main
    use, intrinsic:: iso_fortran_env, only: int32, int64, real64
    USE_FORTRAN_LIB_H
    use, non_intrinsic:: comparable_lib, only: almost_equal
-   use, non_intrinsic:: quadpack_lib
+   use, non_intrinsic:: ad_lib
+   use, non_intrinsic:: quadpack_lib, only: qags
 
    implicit none
 
    Real(kind=real64), parameter:: pi = 2*atan2(1d0, 0d0)
    Integer(kind=int32):: err, n_eval
    Real(kind=real64):: ret, abs_err
-
+   type(Dual64_2_2):: dual_ret, dual_abs_err
 
    call qags(my_sin, 0d0, pi, ret, n_eval=n_eval, err=err, abs_err=abs_err)
    TEST(err == 0)
    TEST(almost_equal(ret, 2d0, atol=abs_err))
+
+   call qags(dual_3sin, 0d0, pi, dual_ret, n_eval=n_eval, err=err, abs_err=dual_abs_err)
+   TEST(err == 0)
+   TEST(almost_equal(real(dual_ret), 6d0, atol=real(dual_abs_err)))
+   TEST(all(almost_equal(grad(dual_ret), 2d0, atol=grad(dual_abs_err))))
 
    call qags(inv_x, 1d0/2, 2d0, ret, n_eval=n_eval, err=err, abs_err=abs_err)
    TEST(err == 0)
@@ -65,4 +71,10 @@ contains
       ret = args(1)/x
    end function inv_x_args
 
+   pure function dual_3sin(x) result(ret)
+      Real(kind=real64), intent(in):: x
+      type(Dual64_2_2):: ret
+
+      ret = sin(x)*Dual64_2_2(3, 1)
+   end function dual_3sin
 end program main
